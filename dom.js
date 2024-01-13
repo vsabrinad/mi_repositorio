34fc579a65280dc7f304bbfbb89e455c6de615e2
Sabrina <sabrinadvillarreal@hotmail.com>
2024-01-13 16:15:47 -0300

    intento fallido de la pre entrega 3

---
+++ dom.js
@@ -0,0 +1,17 @@
+/*let saludar = document.getElementById("ingreso a la pag web");
+let seccion = prompt ("Ingrese la seccion a ingresar");
+
+if (seccion === "formulario"){
+    saludar.innerHTML = `<h1>Bienvenidos a ${seccion}</h1>`;
+    saludar.classname = "rojo";
+} else if ( seccion === "recetas saladas"){
+    saludar.innerHTML = `<h1>Bienvenidos a ${seccion}</h1>`;
+    saludar.className = "naranja";
+} else if ( seccion === "recetas dulces"){
+    saludar.innerHTML = `<h1>Bienvenidos a ${seccion}</h1>`;
+    saludar.className = "amarillo";
+} else if ( seccion === "otros"){
+    saludar.innerHTML = `<h1>Bienvenidos a ${seccion}</h1>`;
+    saludar.className = "marron";
+}
+*/
---
+++ usuario.js
@@ -0,0 +1,164 @@
+/*function solicitarNombreYApellido(){
+   let nombreApellido = prompt ("Ingresar nombre y apellido.").toLowerCase();
+   if ((nombreApellido.trim != "")){
+      alert("Nombre y apellido: " + nombreApellido);
+      mostrarMenuDeInscripcion();
+   } else {
+      alert("¡Nombre y Apellido obligatorio!");
+      solicitarNombreYApellido();
+   }
+}
+solicitarNombreYApellido()
+
+/*PROFE: ACA LE DI UN EJEMPLO UTILIZANDO LA FUNCION DE ARRIBA SOBRE EL USO DEL OPERADOR TERNARIO.
+ 
+ let nombreApellido = prompt ("Ingresar nombre y apellido.").toLowerCase();
+ 
+ nombreApellido.trim != "" ? alert("Nombre y Apellido: " + nombreApellido) : alert("¡Nombre y Apellido obligatorio!");
+*/
+ /*
+function mostrarMenuDeInscripcion(){
+   let inscripcion = prompt ("Se dan 100 cupos por clase. Elija una clase: MARTES o JUEVES").toLowerCase();
+   if ((inscripcion.trim === "martes")){
+      alert("Iniciando su reserva.");
+      mostrarMenuDeOpcionesDeMartes();
+   } else ((inscripcion.trim === "jueves")); {
+      alert("Iniciando su reserva.");
+      mostrarMenuDeOpcionesDeJueves();
+   }
+}
+mostrarMenuDeInscripcion()
+ 
+function mostrarMenuDeOpcionesDeMartes(){
+   let mensaje = prompt("10hs: clase de RECETA SALADA o 12hs: clase RECETA DULCE. Elija la clase.").toLowerCase();
+   if (mensaje === "receta dulce") {
+      alert("¡Excelente!");
+      mostrarOpcionMasVotada();
+   } else if (mensaje === "receta salada") {
+      alert("¡Excelente!");
+      mostrarOpcionMasVotada();
+   } else {
+      alert ("Opcion incorrecta (receta dulce/ receta salada).");
+      mostrarMenuDeOpcionesDeMartes();
+   }
+}
+mostrarMenuDeOpcionesDeMartes()
+ 
+function mostrarMenuDeOpcionesDeJueves(){
+   let mensaje = prompt("10hs: clase de RECETA SALADA o 12hs: clase RECETA DULCE. Elija la clase.").toLowerCase();
+   if (mensaje === "receta dulce") {
+      alert("Excelente.");
+      mostrarOpcionMasVotada();
+   } else if (mensaje === "receta salada") {
+      alert("Excelente.");
+      mostrarOpcionMasVotada();
+   } else {
+      alert ("Opcion incorrecta. Elija receta dulce o receta salada, por favor.)");
+      mostrarMenuDeOpcionesDeJueves();
+   }
+}
+mostrarMenuDeOpcionesDeJueves()
+ 
+function mostrarOpcionMasVotada(){
+   let entrada = prompt("Clase dedicada a receta: Comun, Sintacc o Veggie. La clase se basara en la opcion mas votada.").toLowerCase();
+   let fallos= 0;
+   switch (entrada) {
+      case "comun":
+         alert("Perfecto.");
+         mostrarfinalizacionDelFormulario();
+         break;
+      case "sintacc":
+         alert("Genial.");
+         mostrarfinalizacionDelFormulario();
+         break;
+      case "veggie":
+         alert("Excelente.");
+         mostrarfinalizacionDelFormulario();
+         break;
+      default:
+         alert("Su respuesta no es valida.");
+         mostrarOpcionMasVotada();
+         fallos +=1;
+         break;
+      mostrarOpcionMasVotada();
+   }
+}
+mostrarOpcionMasVotada()
+ 
+function mostrarfinalizacionDelFormulario(){
+   for (let i = 1; i <= 101; i++) {
+      let asignarCupo = prompt("Ingresar correo electronico.");
+      alert("Usted ha sido registrado exitosamente. Cupo nro "+i +" correo: " + asignarCupo);
+      solicitarNombreYApellido();
+   } 
+}
+ 
+//Profe: A partir de aca es sobre la segunda. 
+ 
+// VERIFICACIONES
+ 
+const inscriptosMartes = [ "susana", "carlos", "sofia", "marcelo", "rocio", "paula", "silvia", "omar", 
+    "gustavo", "alan", "sebastian", "carla", "ariel", "nadia", "melisa"];
+    inscriptosMartes.push("elena");
+console.log(inscriptosMartes);
+ 
+    inscriptosMartes.shift("susana");
+console.log(inscriptosMartes);
+ 
+const todosLosInscriptos = ["susana", "carlos", "sofia", "marcelo", "rocio", "paula", "silvia", "omar", 
+ "gustavo", "alan", "sebastian", "carla", "ariel", "nadia", "melisa", "ramiro", "guillermo", "ayelen", "christian", "lidia", "sabrina","cecilia"];
+ 
+console.log(todosLosInscriptos.indexOf("sofia"));
+console.log(todosLosInscriptos.indexOf("cecilia"));
+ 
+const recetaSalada= ["jueves-receta salada"]
+const inscriptosJueves = ["ramiro", "guillermo", "ayelen", "christian", "lidia", "sabrina","cecilia"];
+ 
+const varios= recetaSalada.concat(inscriptosJueves);
+console.log(varios);
+
+const inscriptos = ["susana", "carlos", "sofia", "marcelo", "rocio", "paula", "silvia", "omar", 
+ "gustavo", "alan", "sebastian", "carla", "ariel", "nadia", "melisa", "ramiro", "guillermo", "ayelen", "christian", "lidia", "sabrina","cecilia"];
+ let verificar = prompt ("Verificacion de solicitud. Ingrese el nombre a buscar:");
+ if(inscriptos.includes(verificar)){
+   alert("Inscripto/a.");
+ }else {
+   alert("No inscripto/a.");
+   }
+verificar = prompt("Verificacion de solicitud. Ingrese el nombre a buscar:");
+
+
+const inscripciones = [
+    {nombre: "susana", dia: "martes", horario: 10, receta: "salada"},
+    {nombre: "carlos", dia: "martes", horario: 10, receta:"salada"},
+    {nombre: "sofia", dia: "martes", horario: 12, receta: "dulce"}, 
+    {nombre: "christian", dia: "jueves", horario: 10, receta: "salada"},
+    {nombre: "guillermo", dia: "jueves", horario: 12, receta: "dulce"},
+    {nombre: "ayelen", dia: "jueves", horario: 12, receta: "dulce"},
+    {nombre: "melisa", dia: "martes", horario: 10, receta: "salada"},
+    {nombre: "lidia", dia: "jueves", horario: 12, receta: "dulce"},
+]
+let dia = String(prompt("Para saber dia de su clase. Ingrese su nombre por favor:"));
+const encontrados = inscripciones.filter(item => item.dia = "martes");
+ 
+encontrados.forEach(item => {
+   alert(
+     `Nombre: ${item.nombre} 
+    Dia: ${item.dia}`
+   );
+})
+ 
+const claseMartes = new Date ("january 16, 2024");
+//console.log(claseMartes);
+//console.log(claseMartes.getFullYear());
+const hoy = new Date ("january 12, 2024");
+const miliSegundosPorDia = 86400000
+console.log((claseMartes - hoy) / miliSegundosPorDia);
+ 
+//Profe: A partir de aca es sobre la tercera. 
+*/
+let boton = document.getElementsByClassName("bottom");
+const responder = () => {
+   console.log("Hola. Has hecho click");
+}
+boton.addEventListener("click");
